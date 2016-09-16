8b836711e1c1fbb97c33566f24f36f190b06a0fb
Roland Pangu <faruzzy@gmail.com>
2016-09-16 14:37:11 -0700

    Added exercise 5-16

---
+++ src/com/faruzzy/deitel/chapt5/Exercise_5_16.java
@@ -0,0 +1,17 @@
+package com.faruzzy.deitel.chapt5;
+
+/**
+ * @author Roland
+ */
+public class Exercise_5_16 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+            String s = "";
+
+            for (int k = 1; k <= i; k++)
+                s += "*";
+
+            System.out.printf("%2d: %s\n", i, s);
+        }
+    }
+}
