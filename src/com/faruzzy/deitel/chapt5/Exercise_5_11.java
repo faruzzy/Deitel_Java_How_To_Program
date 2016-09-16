0285367860c3dd010eaca88b7ddc068df4a3697e
Roland Pangu <faruzzy@gmail.com>
2016-09-16 13:20:09 -0700

    Added exercise 5-11

---
+++ src/com/faruzzy/deitel/chapt5/Exercise_5_11.java
@@ -0,0 +1,32 @@
+package com.faruzzy.deitel.chapt5;
+
+import java.util.Scanner;
+
+/**
+ * @author Roland
+ */
+public class Exercise_5_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the number of input: ");
+        int input = Integer.parseInt(scanner.next());
+
+        System.out.println();
+        int lowestValue = 0;
+
+        for (int i = 0; i < input; i++) {
+            System.out.print("Input " + (i + 1) + ": ");
+            int userInput = Integer.parseInt(scanner.next());
+            if (i == 0) {
+                lowestValue = userInput;
+                continue;
+            }
+
+            if (userInput < lowestValue)
+                lowestValue = userInput;
+        }
+
+        System.out.println("Lowest value is " + lowestValue);
+
+    }
+}
