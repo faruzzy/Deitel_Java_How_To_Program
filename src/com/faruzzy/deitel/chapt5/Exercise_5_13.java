9a93f6f411188823290fdd7f1d593abda2cfc9f9
Roland Pangu <faruzzy@gmail.com>
2016-09-16 13:34:40 -0700

    Added exercise 5-13

---
+++ src/com/faruzzy/deitel/chapt5/Exercise_5_13.java
@@ -0,0 +1,26 @@
+package com.faruzzy.deitel.chapt5;
+
+/**
+ * @author Roland
+ */
+public class Exercise_5_13 {
+    private static int factorials(int value) {
+        int result = value;
+        if (value <= 1)
+            return 1;
+        else {
+            for (int k = value - 1; k > 0; k--) {
+                result *= k;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("%s %5s\n", "Value", "Factorial");
+        for (int i = 1; i <= 5; i++) {
+            System.out.printf("%d %5d\n", i, factorials(i));
+        }
+    }
+}
