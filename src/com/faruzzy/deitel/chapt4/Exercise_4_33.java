6113430608d7d760bc8827472815fd341d9c6224
Roland Pangu <faruzzy@gmail.com>
2016-09-17 23:32:44 -0700

    added exercise 4-33

---
+++ src/com/faruzzy/deitel/chapt4/Exercise_4_33.java
@@ -0,0 +1,14 @@
+package com.faruzzy.deitel.chapt4;
+
+/**
+ * @author Roland
+ */
+public class Exercise_4_33 {
+    public static void main(String[] args) {
+        int current = 2;
+        do {
+            System.out.println(current);
+            current *= 2;
+        } while(current >= 2);
+    }
+}
