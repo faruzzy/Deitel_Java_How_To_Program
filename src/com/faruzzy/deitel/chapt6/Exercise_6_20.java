05cf3a2bdd444ce77dc480847e9c7540372d071a
Roland Pangu <faruzzy@gmail.com>
2016-10-14 15:12:54 -0700

    Initial commit of exercise 6.20

---
+++ src/com/faruzzy/deitel/chapt6/Exercise_6_20.java
@@ -0,0 +1,23 @@
+package com.faruzzy.deitel.chapt6;
+
+import java.util.Scanner;
+
+/**
+ * @author Roland Pangu
+ */
+public class Exercise_6_20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the radius of the Circle: ");
+        int radius = Integer.parseInt(input.next());
+        double area = circleArea(radius);
+
+        System.out.println();
+        System.out.printf("The circle area is: %.2f", area);
+    }
+
+    private static double circleArea(int radius) {
+        return  Math.PI * Math.pow(radius, 2);
+    }
+}
