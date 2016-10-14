a4caef625e47b80bd778d15c68b76e6c70c90731
Roland Pangu <faruzzy@gmail.com>
2016-10-14 15:11:45 -0700

    Put the drawing mechanism in its own method

--- src/com/faruzzy/deitel/chapt6/Exercise_6_18.java
+++ src/com/faruzzy/deitel/chapt6/Exercise_6_18.java
@@ -7,15 +7,19 @@ import java.util.Scanner;
  */
 public class Exercise_6_18 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter the number of rows and columns: ");
-        int val = Integer.parseInt(scanner.next());
+        squareOfAsterisks();
+    }
 
-        System.out.println();
-        for (int k = 1; k <= val; k++) {
-            for (int q = 1; q <= val; q++)
-                System.out.print("*");
+    private static void squareOfAsterisks() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Please enter the size of the square: ");
+            int val = Integer.parseInt(scanner.next());
+
             System.out.println();
+            for (int k = 1; k <= val; k++) {
+                for (int q = 1; q <= val; q++)
+                    System.out.print("*");
+                System.out.println();
+            }
         }
     }
-}
