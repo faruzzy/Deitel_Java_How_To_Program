45411354dc24ef0433d05350b546ad0824732dda
Roland Pangu <faruzzy@gmail.com>
2016-10-15 16:40:34 -0700

    Initial Commit for Exercise 6.24

---
+++ src/com/faruzzy/deitel/chapt6/Exercise_6_24.java
@@ -0,0 +1,22 @@
+package com.faruzzy.deitel.chapt6;
+
+/**
+ * @author Roland Pangu
+ */
+public class Exercise_6_24 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++)
+            if (perfect(i))
+                System.out.println(i);
+    }
+
+    private static boolean perfect(int value) {
+        int total = 0;
+        for (int i = value - 1; i >= 1; i--) {
+            if (value % i == 0)
+                total += i;
+        }
+
+        return total == value;
+    }
+}
