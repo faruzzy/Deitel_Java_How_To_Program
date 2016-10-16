b232614353601d6b179faba398c4b1c61f9a41ab
Roland Pangu <faruzzy@gmail.com>
2016-10-16 00:14:38 -0700

    Initial commit of Exercise 6.26

---
+++ src/com/faruzzy/deitel/chapt6/Exercise_6_26.java
@@ -0,0 +1,26 @@
+package com.faruzzy.deitel.chapt6;
+
+import java.util.Scanner;
+
+/**
+ * @author Roland Pangu
+ */
+public class Exercise_6_26 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a number to be reversed: ");
+        int value = Integer.parseInt(input.next());
+
+        System.out.println();
+        System.out.printf("The reverse of %d is: %s\n", value, reverse(value));
+    }
+
+    private static String reverse(int value) {
+        String val = value + "";
+        String str = "";
+
+        for (int j = val.length() - 1; j >= 0; j--)
+            str += val.charAt(j) + "";
+        return str;
+    }
+}
