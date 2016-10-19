996fe392a44d560fe78c30642bcf186d4a45c6f3
Roland Pangu <faruzzy@gmail.com>
2016-10-18 19:45:00 -0700

    Added Utility for printing regular String

---
+++ src/com/faruzzy/util/u.java
@@ -0,0 +1,18 @@
+package com.faruzzy.util;
+
+/**
+ * @author Roland Pangu
+ */
+public final class u {
+    public static void print(String arg) {
+        System.out.print(arg);
+    }
+
+    public static void println(String arg) {
+        System.out.println(arg);
+    }
+
+    public static void println() {
+        System.out.println();
+    }
+}
